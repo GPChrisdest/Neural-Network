94af393ec4cd74192996369e17eb80e8ef035b7d
Christos C. <achrisan2@gmail.com>
2024-05-25 17:16:52 +0300

    fixes

--- code/pkl_viewer.py
+++ code/pkl_viewer.py
@@ -7,14 +7,14 @@ from random import randint
 
 def view():
     plt.ion()
-    f = gzip.open('mnist.pkl.gz', 'rb')
-    training_data, validation_data, test_data = cPickle.load(f)
+    file = gzip.open('mnist.pkl.gz', 'rb')
+    training_data, validation_data, test_data = cPickle.load(file)
 
     train_x, train_y = test_data
+    number =randint(0,10000)
     while True:
         plt.ion()
-        i=randint(0,10000)
-        plt.imshow(train_x[i].reshape((28,28)), cmap = cm.Greys_r)
+        plt.imshow(train_x[number].reshape((28,28)), cmap = cm.Greys_r)
         plt.show()
         plt.pause(0.001) 
         sleep(3)
@@ -22,14 +22,16 @@ def view():
         print("Do you want to select this photo?")
         print("1. Yes")
         print("2. No")
-        x = int(input(">>>"))
         try:
+            x = int(input(">>>"))
             if x == 1:
                 break
             elif x==2:
+                number = randint(0,10000)
                 continue
         except NameError:
             print("Please enter a valid choice!")
-            view()
-    f.close()
-    return i
+            sleep(2)
+            continue
+    file.close()
+    return number
