6309b537e3c897991e6b660313ab525942b121a7
Christos C. <achrisan2@gmail.com>
2024-06-11 18:39:00 +0300

    fixes to menu

--- code/menu.py
+++ code/menu.py
@@ -1,28 +1,44 @@
-import tkinter as tk
+import Tkinter 
+import tkMessageBox
 
 def create_main_window():
-    # Δημιουργία του κύριου παραθύρου
-    root = tk.Tk()
+    # Dhmioyrgia kyrioy para8yrou
+    root = Tkinter.Tk()
     root.title("My App")
    
-    # Ορισμός του χρώματος φόντου
-    root.configure(bg='#00CED1')  # Γαλαζοπράσινο χρώμα
+    # Orismos xrwmatos
+    root.configure(bg='#00CED1')
    
-    # Ορισμός ελάχιστου μεγέθους παραθύρου
+    # Orismos ellaxistoy mege8ous para8yrou
     root.minsize(300, 100)
    
     root.grid_columnconfigure(0, weight=1)
     root.grid_columnconfigure(1, weight=1)
     root.grid_columnconfigure(2, weight=1)
     root.grid_rowconfigure(0, weight=1)
-   
-    # Δημιουργία κουμπιών με padding για να περιοριστεί το μέγιστο μέγεθος
-    train_button = tk.Button(root, text="Train")
-    check_button = tk.Button(root, text="Check")
-    exit_button = tk.Button(root, text="Exit")
+
+    def train():
+        return 0
+
+    def check():
+        return 0
+
+    def exit():
+        return 0
+       
+    # Dhmiourgia koumpiwn me padding gia na perioristei to megisto mege8os
+    train_button = Tkinter.Button(root, text = "Train", command = train)
+    check_button = Tkinter.Button(root, text = "Check", command = check)
+    exit_button = Tkinter.Button(root, text = "Exit", command = exit)
+
+    train_button.pack()
+    check_button.pack()
+    exit_button.pack()
    
     train_button.grid(row=0, column=0, sticky='nsew', padx=10, pady=10, ipadx=10, ipady=10)
     check_button.grid(row=0, column=1, sticky='nsew', padx=10, pady=10, ipadx=10, ipady=10)
+    exit_button.grid(row=0, column=2, sticky='nsew', padx=10, pady=10, ipadx=10, ipady=10)
+
     exit_button.grid(row=0, column=2, sticky='nsew', padx=10, pady=10, ipadx=10, ipady=10)
    
     return root
@@ -30,3 +46,4 @@ def create_main_window():
 if __name__ == "__main__":
     main_window = create_main_window()
     main_window.mainloop()
+    
