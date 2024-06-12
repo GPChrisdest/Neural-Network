93d98f10d34ede1eb8b2c3cb03c034339ad4b6d6
Georgepaxat <151514404+Georgepaxat@users.noreply.github.com>
2024-06-12 12:41:15 +0300

    buttons connect

--- code/menu.py
+++ code/menu.py
@@ -1,6 +1,7 @@
 import Tkinter 
 import tkMessageBox
 import pkl_viewer as pk
+import network as nt
 
 
 def create_main_window():
@@ -15,31 +16,43 @@ def create_main_window():
     root.minsize(600, 400)
 
     def train():
-        return 0
+        root.destroy()
+        nt.start()
 
     def check():
         root.destroy()
         pk.view()
         check_root =Tkinter.Tk() 
         check_root.title("Second Window")
-        yes_button = Tkinter.Button(check_root, text = "Select this photo", command = train)
-        no_button = Tkinter.Button(check_root, text = "Move to next photo", command = check)
+        check_root.configure(bg='#2E3944')
+        check_root.minsize(300, 300)
+        yes_button=Tkinter.Button(check_root, text="Select this photo", command=train, width=15, height=4, bg="#D3D9D4", font=("Helvetica",10,"bold"))
+        no_button=Tkinter.Button(check_root, text="Move to next photo", command=check, width=15, height=4, bg="#D3D9D4", font=("Helvetica",10,"bold"))
+        exit1_button=Tkinter.Button(check_root, text="Exit", command=lambda: exit(check_root), width=15, height=4, bg="#D3D9D4", font=("Helvetica",10,"bold"))
 
         yes_button.pack()
         no_button.pack()
+        exit1_button.pack()
 
-    def exit():
-        return 0
+        yes_button.grid(row=1, column=0, pady=(0,10))
+        no_button.grid(row=2, column=0, pady=(0,10))
+        exit1_button.grid(row=3, column=0, pady=(0,10))
+        check_root.grid_columnconfigure(0,weight=1)
 
+        return check_root
+
+    def exit(name):
+        name.destroy()
+    #photo layaout
     image=Tkinter.PhotoImage(file="C:\Users\georg\Downloads\LogoVersionEN.gif")
     label=Tkinter.Label(root, image=image, width=350, height=170)
     label.grid(row=0, column=0, padx=5, pady=5, sticky="nw")
     label.image=image
     
-    # Dhmiourgia koumpiwn me padding gia na perioristei to megisto mege8os
-    train_button = Tkinter.Button(root, text = "Train", command = train, width=15, height=2, bg="#D3D9D4", font=("Helvetica",10,"bold"))
-    check_button = Tkinter.Button(root, text = "Find hand\nwritten number", command = check, width=15, height=5, bg="#D3D9D4", font=("Helvetica",10,"bold"))
-    exit_button = Tkinter.Button(root, text = "Exit", command = exit, width=15, height=2, bg="#D3D9D4", font=("Helvetica",10,"bold"))
+    
+    train_button=Tkinter.Button(root, text="Train", command = train, width=15, height=2, bg="#D3D9D4", font=("Helvetica",10,"bold"))
+    check_button=Tkinter.Button(root, text="Find hand\nwritten number", command = check, width=15, height=5, bg="#D3D9D4", font=("Helvetica",10,"bold"))
+    exit_button=Tkinter.Button(root, text="Exit", command=lambda: exit(root), width=15, height=2, bg="#D3D9D4", font=("Helvetica",10,"bold"))
 
     train_button.pack()
     check_button.pack()
